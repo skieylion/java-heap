b90bdc0bc73e3fab187fe7cb243814e169c25c10
ubahhukob@yandex.ru <ubahhukob@ya.ru>
2022-07-29 11:58:19 +0500

    add annotations

--- java-project-parent/heap_app/src/main/java/project/java/App.java
+++ java-project-parent/heap_app/src/main/java/project/java/App.java
@@ -2,22 +2,27 @@ package project.java;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-
-public class App {
-
-    @MyAnno(str = "asd", val = 1)
-    public static void myMeth() {
-
-    }
-
-    public static void main(String[] args) {
-
-    }
-}
+import java.lang.reflect.Method;
 
 @Retention(RetentionPolicy.RUNTIME)
 @interface MyAnno {
     String str();
-
     int val();
 }
+
+public class App {
+
+    @MyAnno(str = "asd", val = 1)
+    public void myMeth() {
+
+    }
+
+    public static void main(String[] args) throws NoSuchMethodException {
+        App app = new App();
+        Class<?> c = app.getClass();
+        Method m = c.getMethod("myMeth");
+        MyAnno myAnno = m.getAnnotation(MyAnno.class);
+        System.out.println("myAnno str=" + myAnno.str() + "; val=" + myAnno.val());
+    }
+}
+
