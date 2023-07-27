c769fc6f9ae0ca39b8989ae4a923c7c5f5de974c
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-07-27 10:32:11 +0500

    main: remove classes from heap_app

--- java-project-parent/heap_app/src/main/java/project/java/App.java
+++ java-project-parent/heap_app/src/main/java/project/java/App.java
@@ -1,11 +1,12 @@
 package project.java;
 
+import java.util.Spliterator;
 import java.util.concurrent.Semaphore;
 
 public class App {
 
 
-    public static void main(String[] args) throws InterruptedException {
-
+    public static void main(String[] args) {
+        Spliterator spliterator;
     }
 }
