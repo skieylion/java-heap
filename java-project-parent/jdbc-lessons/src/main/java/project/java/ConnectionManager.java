dbbaa206762e83c3736ee9617d65184ae575b903
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-01 01:50:16 +0500

    main: Add PropertiesUtil class

--- java-project-parent/jdbc-lessons/src/main/java/project/java/ConnectionManager.java
+++ java-project-parent/jdbc-lessons/src/main/java/project/java/ConnectionManager.java
@@ -6,16 +6,18 @@ import java.sql.SQLException;
 
 public final class ConnectionManager {
 
-    private static final String USERNAME = "test";
-    private static final String PASSWORD = "test";
-    private static final String URL = "jdbc:postgresql://localhost:5434/testdb";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String URL_KEY = "db.url";
 
     private ConnectionManager() {
     }
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY));
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
