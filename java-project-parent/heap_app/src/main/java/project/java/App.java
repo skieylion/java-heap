6eb75b0e22aacd52117f9e1cd726b79c8c438c2e
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-01 23:56:32 +0500

    main: Add methods of the Date

--- java-project-parent/heap_app/src/main/java/project/java/App.java
+++ java-project-parent/heap_app/src/main/java/project/java/App.java
@@ -1,8 +1,20 @@
 package project.java;
 
+import java.util.Date;
+
 public class App {
 
     public static void main(String[] args) {
-        
+        Date date1 = new Date();
+        Date date2 = new Date(date1.getTime() + 45_000);
+        Date date3 = (Date) date2.clone();
+        date3.setTime(date3.getTime() + 10_000);
+        Date date4 = Date.from(date1.toInstant());
+
+        System.out.println(date2.after(date1));
+        System.out.println(date1.before(date2));
+        System.out.println(date1.compareTo(date3));
+        System.out.println(date1.compareTo(date4));
+        System.out.println(date1.hashCode());
     }
 }
