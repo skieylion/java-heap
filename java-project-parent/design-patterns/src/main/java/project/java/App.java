875eabfddeccde92de92e49f1fa02578cf1d023d
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-03 00:26:15 +0500

    main: Add facade pattern for getting frames every 5 seconds from the video

--- java-project-parent/design-patterns/src/main/java/project/java/App.java
+++ java-project-parent/design-patterns/src/main/java/project/java/App.java
@@ -1,15 +1,15 @@
-package project.java.decorator;
+package project.java;
 
 import project.java.decorator.text.processor.HtmlEscapeProcessorDecorator;
 import project.java.decorator.text.processor.LowerCaseProcessorDecorator;
 import project.java.decorator.text.processor.TextProcessor;
 import project.java.decorator.text.processor.WhitespaceRemoverDecorator;
+import project.java.facade.CaptureFrameFacade;
 
 public class App {
     public static void main(String[] array) {
-        var dec1 = new LowerCaseProcessorDecorator();
-        var dec2 = new WhitespaceRemoverDecorator(dec1);
-        var dec3 = new HtmlEscapeProcessorDecorator(dec2);
-        System.out.println(dec3.process("      <html>Hello</html>    "));
+        String pathFrom = "C:\\work\\opencv\\video.mp4";
+        String pathTo = "C:\\work\\opencv\\image";
+        CaptureFrameFacade.createImages(5, pathFrom, pathTo, "jpg");
     }
 }
---
+++ java-project-parent/design-patterns/src/main/java/project/java/facade/CaptureFrameFacade.java
@@ -0,0 +1,29 @@
+package project.java.facade;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.videoio.Videoio;
+
+public class CaptureFrameFacade {
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public static void createImages(int timeStep, String pathFrom, String pathTo, String ext) {
+        VideoCapture videoCapture = new VideoCapture(pathFrom);
+        double frameRate = videoCapture.get(Videoio.CAP_PROP_FPS);
+        int frameSkip = (int) (timeStep * frameRate);
+        int frameCount = 0;
+        Mat frame = new Mat();
+        while (videoCapture.read(frame)) {
+            if (frameCount % frameSkip == 0) {
+                Imgcodecs.imwrite(String.format("%s_%d.%s", pathTo, frameCount, ext), frame);
+            }
+            frameCount++;
+        }
+        videoCapture.release();
+    }
+}
