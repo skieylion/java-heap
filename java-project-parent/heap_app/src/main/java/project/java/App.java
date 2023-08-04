3eb5245f32afb778ab911878f19c858c088b61f3
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-04 23:27:10 +0500

    main: multithreading

--- java-project-parent/heap_app/src/main/java/project/java/App.java
+++ java-project-parent/heap_app/src/main/java/project/java/App.java
@@ -1,7 +1,23 @@
 package project.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
+    public List<Integer> findSubstring(String s, String[] words) {
+        int[] counter = new int[words.length];
+        return new ArrayList<>();
+    }
+
+    //1. алгоритм нарайаны (для реализации перестановок последовательности)
+    //2. паттерн итератор (для обхода всех перестановок)
+
+
     public static void main(String[] args) {
+
+
+
+        new ReentrantLockExample();
     }
 }
