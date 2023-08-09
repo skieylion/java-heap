759ca3b394e48d59ca1c42716514ba5c155cd4a6
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-09 11:48:59 +0500

    main: jdbc

--- java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
+++ java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
@@ -1,14 +1,15 @@
 package project.java;
 
-import org.postgresql.Driver;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        try (Connection connection = ConnectionManager.getConnection()) {
+        String sql = "CREATE DATABASE game;";
+        try (Connection connection = ConnectionManager.getConnection();
+             var statement = connection.createStatement()) {
+            boolean flag = statement.execute(sql);
+            System.out.println(flag);
             System.out.println(connection.getTransactionIsolation());
         }
 
