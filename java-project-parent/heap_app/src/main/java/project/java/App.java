b7ff7aad00ca9bc733a3ffffbf4c45f9bc7cc24a
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-11 23:56:05 +0500

    main:

--- java-project-parent/heap_app/src/main/java/project/java/App.java
+++ java-project-parent/heap_app/src/main/java/project/java/App.java
@@ -1,17 +1,7 @@
 package project.java;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class App {
 
-    public List<Integer> findSubstring(String s, String[] words) {
-        int[] counter = new int[words.length];
-        return new ArrayList<>();
-    }
-
-    //клиентский код
     public static void main(String[] args) {
 
     }
