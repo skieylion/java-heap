49d357970ba2c3f94c17dd627ac33ab68e263f9f
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-14 23:00:26 +0500

    main: Statement. DML operations

--- java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
+++ java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
@@ -5,12 +5,12 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        String sql = "CREATE SCHEMA game_schema;";
+        String sql = "update info set data = 'Test' where id=5;";
         try (Connection connection = ConnectionManager.getConnection();
              var statement = connection.createStatement()) {
-            boolean flag = statement.execute(sql);
-            System.out.println(flag);//false (нет селектов)
-            System.out.println(connection.getTransactionIsolation());
+            int count = statement.executeUpdate(sql);
+            System.out.println(count);//false (нет селектов)
+            System.out.println(statement.getUpdateCount());//количество вставленных страниц
         }
 
     }
