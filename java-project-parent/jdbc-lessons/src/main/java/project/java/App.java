46014d90351a073f9c62c170d85b3907e1269d43
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-14 22:11:14 +0500

    main: clear conntect to postgresql

--- java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
+++ java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
@@ -5,13 +5,7 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        String sql = "CREATE DATABASE game;";
-        try (Connection connection = ConnectionManager.getConnection();
-             var statement = connection.createStatement()) {
-            boolean flag = statement.execute(sql);
-            System.out.println(flag);
-            System.out.println(connection.getTransactionIsolation());
-        }
+
 
     }
 }
