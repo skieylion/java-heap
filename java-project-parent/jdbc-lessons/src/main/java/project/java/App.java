7f2be1b641227c66c726e1d19faa084249670a0b
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-14 23:53:46 +0500

    main: preview ResultSet

--- java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
+++ java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
@@ -1,16 +1,18 @@
 package project.java;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-        String sql = "";
+        String sql = "select * from ticket";
         try (Connection connection = ConnectionManager.getConnection();
              var statement = connection.createStatement()) {
-            int count = statement.executeUpdate(sql);
-            System.out.println(count);
-            System.out.println(statement.getUpdateCount());
+            ResultSet executeResult = statement.executeQuery(sql);
+            while (executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+            }
         }
     }
 }
