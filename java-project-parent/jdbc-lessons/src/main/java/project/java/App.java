94a874cae1c8b52ce721d129b7be421f61c797ec
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-14 22:33:25 +0500

    main: Statement. DDL operations

--- java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
+++ java-project-parent/jdbc-lessons/src/main/java/project/java/App.java
@@ -5,7 +5,13 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException {
-
+        String sql = "CREATE SCHEMA game_schema;";
+        try (Connection connection = ConnectionManager.getConnection();
+             var statement = connection.createStatement()) {
+            boolean flag = statement.execute(sql);
+            System.out.println(flag);//false (нет селектов)
+            System.out.println(connection.getTransactionIsolation());
+        }
 
     }
 }
