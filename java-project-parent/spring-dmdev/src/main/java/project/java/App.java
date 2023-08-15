32b6d0f4d1ae48e780ed67ac6caf580cdc9e8bb1
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-15 23:35:11 +0500

    main: Factory Method Injection

--- java-project-parent/spring-dmdev/src/main/java/project/java/App.java
+++ java-project-parent/spring-dmdev/src/main/java/project/java/App.java
@@ -8,6 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
+        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
+        System.out.println(companyRepository);
         System.out.println(context.getBean("pool1", ConnectionPool.class));
     }
 }
---
+++ java-project-parent/spring-dmdev/src/main/java/project/java/CompanyRepository.java
@@ -0,0 +1,13 @@
+package project.java;
+
+public class CompanyRepository {
+    private final ConnectionPool connectionPool;
+
+    public CompanyRepository(ConnectionPool connectionPool) {
+        this.connectionPool = connectionPool;
+    }
+
+    public static CompanyRepository of(ConnectionPool connectionPool) {
+        return new CompanyRepository(connectionPool);
+    }
+}
