d3678272e9665877d9feb5c96a21cf62bfb3ea05
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-16 20:00:24 +0500

    main: stream api

--- java-project-parent/heap_app/src/main/java/project/java/App.java
+++ java-project-parent/heap_app/src/main/java/project/java/App.java
@@ -1,8 +1,17 @@
 package project.java;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class App {
 
     public static void main(String[] args) {
-
+        List<String> array = List.of("H", "e", "l", "l", "o", "W", "o", "r", "l", "d");
+        var result = array.stream()
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+        System.out.println(result);
     }
 }
