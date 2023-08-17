5ad64b43c92991e771766974348fd4f070d5c5ca
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-17 23:38:39 +0500

    main: init and destroy callbacks

--- java-project-parent/spring-dmdev/src/main/java/project/java/App.java
+++ java-project-parent/spring-dmdev/src/main/java/project/java/App.java
@@ -1,21 +1,15 @@
 package project.java;
 
-import org.springframework.beans.factory.config.CustomScopeConfigurer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.SimpleThreadScope;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        var context = new ClassPathXmlApplicationContext("application.xml");
-        var myThread1 = context.getBean("myThread", MyThread.class);
-        var myThread2 = context.getBean("myThread", MyThread.class);
-        if (myThread1 != myThread2) {
-            System.out.println("myThread1!=myThread2");
+        try (var context = new ClassPathXmlApplicationContext("application.xml")) {
+            var myThread1 = context.getBean("myThread", MyThread.class);
+            var myThread2 = context.getBean("myThread", MyThread.class);
+            if (myThread1 != myThread2) {
+                System.out.println("myThread1!=myThread2");
+            }
         }
     }
 }
--- java-project-parent/spring-dmdev/src/main/java/project/java/MyThread.java
+++ java-project-parent/spring-dmdev/src/main/java/project/java/MyThread.java
@@ -1,4 +1,12 @@
 package project.java;
 
 public class MyThread {
+
+    private void init() {
+        System.out.println("init MyThread");
+    }
+
+    private void destroy() {
+        System.out.println("destroy MyThread");
+    }
 }
