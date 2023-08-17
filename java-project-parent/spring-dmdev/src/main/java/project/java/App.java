e1cf73ece0d28caa869ef5e21b8907934d4164f5
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-17 22:54:23 +0500

    main: Scope thread

--- java-project-parent/spring-dmdev/src/main/java/project/java/App.java
+++ java-project-parent/spring-dmdev/src/main/java/project/java/App.java
@@ -1,6 +1,10 @@
 package project.java;
 
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.SimpleThreadScope;
 
 /**
  * Hello world!
@@ -8,8 +12,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
-        var companyRepository = context.getBean("companyRepository", CompanyRepository.class);
-        System.out.println(companyRepository);
-        System.out.println(context.getBean("pool1", ConnectionPool.class));
+        var myThread1 = context.getBean("myThread", MyThread.class);
+        var myThread2 = context.getBean("myThread", MyThread.class);
+
+        new Thread(() -> {
+            var myThread3 = context.getBean("myThread", MyThread.class);
+            if (myThread1 == myThread2) {
+                System.out.println("myThread1=myThread2");
+            }
+            if (myThread3 != myThread1) {
+                System.out.println("myThread3!=myThread1");
+            }
+            System.out.println("end");
+        }, "thread1").start();
     }
 }
