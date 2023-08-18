5293af3f9b707c9f6ffff43da0a2a9c2dc1e8d23
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-18 10:40:13 +0500

    main: SpEL

--- java-project-parent/spring-dmdev/src/main/java/project/java/App.java
+++ java-project-parent/spring-dmdev/src/main/java/project/java/App.java
@@ -1,15 +1,14 @@
 package project.java;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 public class App {
     public static void main(String[] args) {
+        PropertySourcesPlaceholderConfigurer s;
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
-            var myThread1 = context.getBean("myThread", MyThread.class);
-            var myThread2 = context.getBean("myThread", MyThread.class);
-            if (myThread1 != myThread2) {
-                System.out.println("myThread1!=myThread2");
-            }
+            var pool = context.getBean("pool1", ConnectionPool.class);
+            System.out.println(pool);
         }
     }
 }
