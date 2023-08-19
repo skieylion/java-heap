566c14b5cd58d42101c2b7828974e6baa9081bea
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-19 15:19:16 +0500

    main: prepare JDBC

--- java-project-parent/heap_app/src/main/java/project/java/App.java
+++ java-project-parent/heap_app/src/main/java/project/java/App.java
@@ -1,17 +1,28 @@
 package project.java;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Locale;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
-
     public static void main(String[] args) {
-        List<String> array = List.of("H", "e", "l", "l", "o", "W", "o", "r", "l", "d");
-        var result = array.stream()
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
-        System.out.println(result);
+        toCollection();
+        joining();
+    }
+
+    static void toCollection() {
+        Set<String> treeSet = Stream.of("D", "X", "F", "A").collect(Collectors.toCollection(TreeSet::new));
+        treeSet.forEach(System.out::print);
+        System.out.println();
+    }
+
+    static void joining() {
+        String symbols = Stream.of("D", "X", "F", "A").collect(Collectors.joining());
+        System.out.println(symbols);
+        String symbols2 = Stream.of("D", "X", "F", "A").collect(Collectors.joining("-"));
+        System.out.println(symbols2);
+        String symbols3 = Stream.of("D", "X", "F", "A").collect(Collectors.joining("-", "[", "]"));
+        System.out.println(symbols3);
     }
 }
