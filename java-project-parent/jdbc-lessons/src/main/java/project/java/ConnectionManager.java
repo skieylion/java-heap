f9104e0b83053243368c9277fc6863a1b44c9841
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-22 00:16:18 +0500

    main: Connection Pool

--- java-project-parent/jdbc-lessons/src/main/java/project/java/ConnectionManager.java
+++ java-project-parent/jdbc-lessons/src/main/java/project/java/ConnectionManager.java
@@ -1,24 +1,73 @@
 package project.java;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public final class ConnectionManager {
 
     private static final String USERNAME_KEY = "db.username";
     private static final String PASSWORD_KEY = "db.password";
     private static final String URL_KEY = "db.url";
+    private static final String POOL_SIZE_KEY = "db.pool.size";
+    private static final int DEFAULT_POOL_SIZE = 10;
+
+    private static ArrayBlockingQueue<Connection> pool;
+
+    static {
+        loadDriver();
+        initConnectionPool();
+    }
 
     private ConnectionManager() {
     }
 
     public static Connection getConnection() {
         try {
+            return pool.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Connection open() {
+        try {
             return DriverManager.getConnection(PropertiesUtil.get(URL_KEY),
                     PropertiesUtil.get(USERNAME_KEY),
                     PropertiesUtil.get(PASSWORD_KEY));
         } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void initConnectionPool() {
+        var poolSize = PropertiesUtil.get(POOL_SIZE_KEY);
+        var size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
+        pool = new ArrayBlockingQueue<>(size);
+        for (int i = 0; i < size; i++) {
+            Connection connection = open();
+            var proxyConnection = (Connection)
+                    Proxy.newProxyInstance(ConnectionManager.class.getClassLoader(), new Class[]{Connection.class},
+                            (proxy, method, args) -> {
+                                if (method.getName().equals("close")) {
+                                    pool.add((Connection) proxy);
+                                    return null;
+                                }
+                                return method.invoke(connection, args);
+                            });
+            pool.add(proxyConnection);
+        }
+    }
+
+
+    private static void loadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
