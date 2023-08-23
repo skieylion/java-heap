fc33985c33727b8b2ab7029e6255b3021c65d86a
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-23 22:55:39 +0500

    main: Stream API. Collectors. Collecting and then

--- java-project-parent/stream-api/src/main/java/project/java/App.java
+++ java-project-parent/stream-api/src/main/java/project/java/App.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
@@ -66,6 +67,15 @@ public class App {
                 .collect(Collectors.averagingInt(Student::getAge));
         System.out.println(averageAge);
     }
+
+    //дополнительное преобразование
+    static void collectingAndThen() {
+        var students = Stream.of(new Student("Вася", 10), new Student("Петя", 19),
+                        new Student("Вова", 67), new Student("Майкл", 29))
+                .collect(Collectors.collectingAndThen(Collectors.toList(),
+                        Collections::unmodifiableList));
+    }
+
 }
 
 @Getter
