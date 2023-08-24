d864fa561f603f446f49d46661fa5c565b492ce5
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-08-24 23:45:21 +0500

    main: Spring Core. XML Configuration. Ordered

--- java-project-parent/spring-dmdev/src/main/java/project/java/MyBeanFactoryPostProcessor.java
+++ java-project-parent/spring-dmdev/src/main/java/project/java/MyBeanFactoryPostProcessor.java
@@ -3,12 +3,19 @@ package project.java;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
 
-public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor, PriorityOrdered {
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
         var bean = configurableListableBeanFactory.getBean("myThread", MyThread.class);
         System.out.println("BEAN");
         System.out.println(bean.getName());
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 }
