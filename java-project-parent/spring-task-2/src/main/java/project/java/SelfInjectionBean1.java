2b651e93b54cc74127a7f078a4f2c8f5b5295bf4
e.ivannikov <e.ivannikov@itfbgroup.ru>
2023-10-06 11:47:02 +0500

    main: @Value with default value and mapping

--- java-project-parent/spring-task-2/src/main/java/project/java/SelfInjectionBean1.java
+++ java-project-parent/spring-task-2/src/main/java/project/java/SelfInjectionBean1.java
@@ -1,17 +1,41 @@
 package project.java;
 
 import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Component
 @Scope("prototype")
 public abstract class SelfInjectionBean1 {
+    @Value("${app.sentence}")
+    private String sentence;
+
+    @Value("${app.sentence.default:Hi World}")
+    private String defaultSentence;
+
+    @Value("${app.names}")
+    private String[] names;
+
+    @Value("${app.names}")
+    private List<String> listOfNames;
+
+    @Value("#{${app.coords}}")
+    private Map<String, Integer> coords;
 
     @Lookup
     public abstract SelfInjectionBean1 self();
 
     public void log() {
-        System.out.println("self bean 1 : "+self());
+        System.out.println("self bean 1 : " + self());
+        System.out.println("sentence : " + sentence);
+        System.out.println("defaultSentence : " + defaultSentence);
+        System.out.println("names : " + Arrays.toString(names));
+        System.out.println("coords : " + coords);
+        System.out.println("list of names : " + listOfNames);
     }
 }
--- java-project-parent/spring-task-2/src/main/java/project/java/SelfInjectionBean2.java
+++ java-project-parent/spring-task-2/src/main/java/project/java/SelfInjectionBean2.java
@@ -4,17 +4,23 @@ import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+
 @Component
 @Scope("prototype")
 public class SelfInjectionBean2 {
+
+    @Resource(name = "selfInjectionBean3")
+    private SelfInjectionBean3 selfInjectionBean3;
+
     @Lookup
-    public SelfInjectionBean1 self() {
+    public SelfInjectionBean2 self() {
         return null;
     }
 
-    ;
 
     public void log() {
         System.out.println("self bean 2 : " + self());
+        selfInjectionBean3.log();
     }
 }
